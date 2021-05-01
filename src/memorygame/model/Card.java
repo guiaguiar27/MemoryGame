fb8cfb921ce3e618f8b8b7724ad76c678139845a
Luciano <luciano.alcantara@ufv.br>
2021-05-01 19:16:28 -0300

    add changes

--- src/memorygame/model/Card.java
+++ src/memorygame/model/Card.java
@@ -1,93 +1,137 @@
 package memorygame.model;
 
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import javax.swing.Icon;
-import javax.swing.JLabel;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Card extends JLabel implements MouseListener {
-    
+
     Icon faceIcon;
     Icon backIcon;
-    int num;
-    int iconWithHalf;
-    int iconHeightHalf;
     boolean faceUp = false;
-    boolean mousePressOnMe = false;
-    
+    int num;
+    int iconWidthHalf, iconHeightHalf;
+    boolean mousePressedOnMe = false;
+
     public Card(Icon face, Icon back, int num) {
+        // inicialmente mostrar a face para baixo
         super(back);
         this.faceIcon = face;
         this.backIcon = back;
         this.num = num;
-
-        //pegar cliques do mouse
+        // capturar cliques do mouse e eventos
         this.addMouseListener(this);
+      
+        this.iconWidthHalf = back.getIconWidth() / 2;
         this.iconHeightHalf = back.getIconHeight() / 2;
-        this.iconWithHalf = face.getIconWidth() / 2;
     }
-    
+
+    public void turnUp() {
+        if (this.faceUp) {
+            return;
+        }
+
+        this.faceUp = true;
+
+        if (this.faceUp) {
+            this.setIcon(this.faceIcon);
+        }
+    }
+
+    public void turnDown() {
+
+        if (!this.faceUp) {
+            return;
+        }
+
+        this.setIcon(this.backIcon);
+
+        this.faceUp = false;
+    }
+
+    /**
+     * devolve o número que identifica o tipo de cartão (qual o lado)
+     *
+     * @return
+     */
     public int getNum() {
         return num;
     }
-    
+
+    /**
+     * Verifique se as coordenadas estão sobre o ícone
+     *
+     * @param x coordanada X
+     * @param y coordenada Y
+     * @return verdadeiro se as coordenadas estiverem sobre o ícone, caso
+     * contrário, falso
+     */
     private boolean overIcon(int x, int y) {
-        int distanceX = Math.abs(x - (this.getWidth() / 2));
-        int distanceY = Math.abs(y - (this.getHeight() / 2));
-        
-        return !(distanceX > this.iconHeightHalf || distanceY > this.iconWithHalf);
+        // calcula a distância do centro do Card
+        int distX = Math.abs(x - (this.getWidth() / 2));
+        int distY = Math.abs(y - (this.getHeight() / 2));
+
+        return !(distX > this.iconWidthHalf || distY > this.iconHeightHalf);
     }
 
-    //é chamado uma vez que há um movimento do mouse dentro do quadro.
+    /**
+     * /é chamado uma vez que há um movimento do mouse dentro do quadro.
+     *
+     * @param e objeto que contém informações sobre o clique do botão
+     */
     @Override
-    public void mouseClicked(MouseEvent me) {
-        if (overIcon(me.getX(), me.getY())) {
+    public void mouseClicked(MouseEvent e) {
+        // over icon, so try to turn up the card
+        if (overIcon(e.getX(), e.getY())) {
             this.turnUp();
         }
     }
 
-    //é chamado quando o mouse é pressionado
+    /**
+     * é chamado quando o mouse é pressionado
+     *
+     * @param e objeto que contém informações sobre o clique do botão
+     */
     @Override
-    public void mousePressed(MouseEvent me) {
-        if (overIcon(me.getX(), me.getY())) {
-            this.mousePressOnMe = true;
+    public void mousePressed(MouseEvent e) {
+        // over icon, so remember this is a mouse press
+        if (overIcon(e.getX(), e.getY())) {
+            this.mousePressedOnMe = true;
         }
     }
 
-    //é chamado quando o botão do mouse é liberado
+    /**
+     * é chamado quando o botão do mouse é liberado
+     *
+     * @param e objeto que contém informações sobre o clique do botão
+     */
     @Override
-    public void mouseReleased(MouseEvent me) {
-        if (this.mousePressOnMe) {
-            this.mousePressOnMe = false;
-            this.mouseClicked(me);
+    public void mouseReleased(MouseEvent e) {
+        // previous press was over icon
+        if (this.mousePressedOnMe) {
+            // mouse is no longer pressed
+            this.mousePressedOnMe = false;
+            // it was a click, so treat it as one
+            this.mouseClicked(e);
         }
     }
 
-    //é chamado quando o mouse vem de uma janela exterior ao quadro.
+    /**
+     * é chamado quando o mouse vem de uma janela exterior ao quadro.
+     *
+     * @param e objeto que contém informações sobre o clique do botão
+     */
     @Override
-    public void mouseEntered(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void mouseEntered(MouseEvent e) {
     }
 
-    //é chamado quando o mouse passa fora do quadro i.e. JFrame
+    /**
+     * é chamado quando o mouse passa fora do quadro i.e. JFrame
+     *
+     * @param e objeto que contém informações sobre o clique do botão
+     */
     @Override
-    public void mouseExited(MouseEvent me) {
-        this.mousePressOnMe = false;
-    }
-    
-    private void turnUp() {
-        if (this.faceUp) return;
-        if (this.faceUp) {
-            this.setIcon(this.faceIcon);
-        }
-        
-    }
-
-    private void turnDown() {
-        if (this.faceUp) {
-            this.setIcon(this.backIcon);
-            this.faceUp = false;
-        }
-        
+    public void mouseExited(MouseEvent e) {
+        // forget any previous mouse press
+        this.mousePressedOnMe = false;
     }
 }
--- src/memorygame/view/MainView.java
+++ src/memorygame/view/MainView.java
@@ -1,28 +1,136 @@
 package memorygame.view;
 
 import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.MediaTracker;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import memorygame.model.Card;
 
-public class MainView {
-    
+public class MainView implements ActionListener {
+
     private final JFrame mainframe;
-    private Container mainContentPane;
-    private ImageIcon cardIcon[];
-    
+    private final Container mainContentPane;
+    private final ImageIcon cardIcon[]; //0-7 faces, 8 trás
+    private final String ImageDirectory = "default";
+
     public MainView() {
-       
+
         this.mainframe = new JFrame("Jogo da Memória");
         this.mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.mainframe.setSize(800, 900);
         this.mainframe.setLocationRelativeTo(null);
         this.mainContentPane = this.mainframe.getContentPane();
         this.mainContentPane.setLayout(new BoxLayout(this.mainContentPane, BoxLayout.PAGE_AXIS));
+
+        JMenuBar menuBar = new JMenuBar();
+        this.mainframe.setJMenuBar(menuBar);
+
+        JMenu gameMenu = new JMenu("Opções");
+        menuBar.add(gameMenu);
+
+        newMenuItem("Novo Jogo", gameMenu, this);
+        newMenuItem("Sair", gameMenu, this);
+
+        JMenu aboutMenu = new JMenu("Sobre");
+        menuBar.add(aboutMenu);
+        newMenuItem("Ajuda", aboutMenu, this);
+        newMenuItem("Sobre", aboutMenu, this);
+        newMenuItem("Outros", aboutMenu, this);
+
+        this.cardIcon = loadCardIcons();
     }
-    
-    public void newGame(){
+
+    private ImageIcon[] loadCardIcons() {
+        ImageIcon[] icon = new ImageIcon[9];
+
+        for (int i = 0; i < 9; i++) {
+            String fileName = "images/" + ImageDirectory + "/card-" + i + ".png";
+            icon[i] = new ImageIcon(fileName);
+
+            if (icon[i].getImageLoadStatus() == MediaTracker.ERRORED) {
+                JOptionPane.showMessageDialog(this.mainframe,
+                        "The image " + fileName + " could not be loaded.",
+                        "Matching Game Error", JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+            }
+        }
+
+        return icon;
+    }
+
+    public static void randomizeIntArray(int[] a) {
+        Random randomizer = new Random();
+
+        for (int i = 0; i < a.length; i++) {
+
+            int d = randomizer.nextInt(a.length);
+
+            int t = a[d];
+            a[d] = a[i];
+            a[i] = t;
+        }
+    }
+
+    public JPanel makeCards() {
+
+        JPanel panel = new JPanel(new GridLayout(4, 2));
+
+        // define a face de trás
+        ImageIcon backIcon = this.cardIcon[8];
+
+        int cardsToAdd[] = new int[8];
+        for (int i = 0; i < 4; i++) {
+            cardsToAdd[2 * i] = i;
+            cardsToAdd[2 * i + 1] = i;
+        }
+
+        randomizeIntArray(cardsToAdd);
+
+        for (int i = 0; i < cardsToAdd.length; i++) {
+
+            int num = cardsToAdd[i];
+
+            Card newCard = new Card(this.cardIcon[num], backIcon, num);
+
+            panel.add(newCard);
+        }
+
+        return panel;
+    }
+
+    private void newMenuItem(String string, JMenu gameMenu, ActionListener listener) {
+        JMenuItem newItem = new JMenuItem(string);
+        newItem.setActionCommand(string);
+        newItem.addActionListener(listener);
+        gameMenu.add(newItem);
+
+    }
+
+    public void newGame() {
+        this.mainContentPane.removeAll();
+
+        this.mainContentPane.add(makeCards());
+
         this.mainframe.setVisible(true);
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getActionCommand().equals("Novo Jogo")) {
+            newGame();
+        }
+        if (ae.getActionCommand().equals("Sair")) {
+            System.exit(0);
+        }
+    }
 }

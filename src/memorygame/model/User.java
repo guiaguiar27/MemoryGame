b77558a26a21377e619ddb2153435e141290d901
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-05-02 17:05:39 -0300

    Update User.java

--- src/memorygame/model/User.java
+++ src/memorygame/model/User.java
@@ -1,4 +1,6 @@
 package memorygame.model;
+import memorygame.Controller.GameController;
+
 import java.util.ArrayList;
 
 public class User {
@@ -10,12 +12,16 @@ public class User {
         return Score;
     }
 
-    public void setScore(int score) {
-        this.Score += score;
+    public void setScore(int Score) {
+        this.Score = Score;
     }
-    private void AddPairCars(Card C1, Card C2){
+    public void AddPairCards(Card C1, Card C2){
+        int Score;
         FindedCards.add(C1);
         FindedCards.add(C2);
+        Score = FindedCards.size() / 2;
+        setScore(Score);
+
     }
     public boolean win(){
         if(FindedCards.size() == 8) return true;
@@ -37,4 +43,5 @@ public class User {
     public void setType(String type) {
         Type = type;
     }
+
 }
